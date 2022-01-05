6fd4476954b206ddc3adad1ce7966b2551d3ed2b
Yuriy Koziy <yukoziy@gmail.com>
2022-01-05 12:40:46 -0500

    caesar passes tests

--- code/caesarCipher.js
+++ code/caesarCipher.js
@@ -1,5 +1,29 @@
+function encrypt(code, key) {
+  let shiftedCode = (code + key) % 26;
+  return String.fromCharCode(shiftedCode + 97);
+}
+
+function isUpperCase(char) {
+  return char === char.toUpperCase();
+}
+
 function caesarCipher(srcString, shiftKey) {
-  return srcString;
+  let chars = srcString.toLowerCase().split('');
+  let result = '';
+  for (let i = 0; i < chars.length; i++) {
+    if (chars[i].match(/[a-z]/g)) {
+      let code = chars[i].charCodeAt(0) - 97;
+      let newChar = encrypt(code, shiftKey);
+      if (isUpperCase(srcString[i])) {
+        result += newChar.toUpperCase();
+      } else {
+        result += newChar;
+      }
+    } else {
+      result += chars[i];
+    }
+  }
+  return result;
 }
 
 export default caesarCipher;
--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -1,17 +1,17 @@
 import caesarCipher from '../code/caesarCipher';
 
 test('defend the east wall of the castle IS efgfoe uif fbtu xbmm pg uif dbtumf with shift of 1', () => {
-  expect(caesarCipher('defend the east wall of the castle')).toBe(
+  expect(caesarCipher('defend the east wall of the castle', 1)).toBe(
     'efgfoe uif fbtu xbmm pg uif dbtumf',
   );
 });
 
 test('Hello world! IS Vszzc kcfzr! with shift of 90,000', () => {
-  expect(caesarCipher('Hello world!')).toBe('Vszzc kcfzr!');
+  expect(caesarCipher('Hello world!', 90000)).toBe('Vszzc kcfzr!');
 });
 
-test('LoOk MoM I cAn WrItE Js. IS YbBx ZbZ V pNa JeVgR Wf. with shift of 90,000', () => {
-  expect(caesarCipher('LoOk MoM I cAn WrItE Js.')).toBe(
+test('LoOk MoM I cAn WrItE Js. IS YbBx ZbZ V pNa JeVgR Wf. with shift of 13', () => {
+  expect(caesarCipher('LoOk MoM I cAn WrItE Js.', 13)).toBe(
     'YbBx ZbZ V pNa JeVgR Wf.',
   );
 });
