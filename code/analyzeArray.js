434565fa00538e2ccebf50f6a0864a226d345e82
Yuriy Koziy <yukoziy@gmail.com>
2022-01-05 12:57:51 -0500

    fix, empty array returns empty object

--- code/analyzeArray.js
+++ code/analyzeArray.js
@@ -1,5 +1,8 @@
 function analyzeArray(srcArray) {
   const result = {};
+  if (srcArray.length == 0) {
+    return result;
+  }
   result.length = srcArray.length;
   result.max = Math.max(...srcArray);
   result.min = Math.min(...srcArray);
--- tests/analyzeArray.test.js
+++ tests/analyzeArray.test.js
@@ -1,7 +1,11 @@
 import analyzeArray from '../code/analyzeArray';
 
-test('lalaal', () => {
+test('analyzeArray returns the correct object for the array [1, 8, 3, 4, 2, 6]', () => {
   const result = { average: 4, min: 1, max: 8, length: 6 };
 
   expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual(result);
+});
+
+test('analyzeArray returns the correct object for the array []', () => {
+  expect(analyzeArray([])).toEqual({});
 });
