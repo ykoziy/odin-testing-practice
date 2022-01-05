acdc83b371564d36fa2997af1dbff3fe1e3dbeeb
Yuriy Koziy <yukoziy@gmail.com>
2022-01-05 11:02:51 -0500

    capitalize is passing tests.

--- code/capitalize.js
+++ code/capitalize.js
@@ -1,3 +1,9 @@
-function capitalize(srcString) {}
+function capitalize(srcString) {
+  if (srcString.length == 0) {
+    return '';
+  }
+  const firstLetter = srcString.slice(0, 1);
+  return firstLetter.toUpperCase() + srcString.slice(1);
+}
 
 export { capitalize };
