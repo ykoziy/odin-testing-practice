8d108329d8a4dfcfe77df60131542d94cd8538e4
Yuriy Koziy <yukoziy@gmail.com>
2022-01-05 11:12:07 -0500

    Reverse passes the tests.

--- code/reverseString.js
+++ code/reverseString.js
@@ -1,3 +1,13 @@
-function reverse(srcString) {}
+function reverse(srcString) {
+  if (srcString.length == 0) {
+    return '';
+  }
+  const chars = srcString.split('');
+  let result = '';
+  for (let i = chars.length - 1; i >= 0; i--) {
+    result += chars[i];
+  }
+  return result;
+}
 
 export { reverse };
